fdd23110972c9dd06791286b2c771af249245542
Bruno Maximiliano <brunomxpirescardoso@gmail.com>
2025-08-01 21:43:51 -0300

    feat(): Criacao metodo validacao vazio ou nulo

--- src/main/java/br/com/bruno/biblioteca/utilitarios/ValidacaoUtil.java
+++ src/main/java/br/com/bruno/biblioteca/utilitarios/ValidacaoUtil.java
@@ -2,4 +2,14 @@ package br.com.bruno.biblioteca.utilitarios;
 
 public class ValidacaoUtil {
 
+    /**
+     * Verifica se o valor é nulo ou vazio;
+     * @param var
+     * @return boolean
+     */
+    public void isNullOrEmpty(String var){
+        if(var == null || var.trim().isEmpty()){
+            throw new IllegalArgumentException("O valor não pode ser nulo ou vazio!");
+        }
+    }
 }

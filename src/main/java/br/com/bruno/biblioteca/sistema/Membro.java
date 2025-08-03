472145fdde2c52f6503ce498feb278b411cea5b2
Bruno Maximiliano <brunomxpirescardoso@gmail.com>
2025-08-03 20:55:26 -0300

    feat(): Cria classe membro Cria classe, variaveis(nome, id) e seus getters/setters.

--- src/main/java/br/com/bruno/biblioteca/sistema/Membro.java
+++ src/main/java/br/com/bruno/biblioteca/sistema/Membro.java
@@ -1,4 +1,32 @@
 package br.com.bruno.biblioteca.sistema;
 
+import br.com.bruno.biblioteca.utilitarios.ValidacaoUtil;
+
 public class Membro {
+        ValidacaoUtil valida;
+
+        private String nome;
+        private int id;
+
+        public Membro(){
+
+        }
+
+        public String getNome(){
+            return this.nome;
+        }
+
+        public void setNome(String nome){
+            valida.isNullOrEmpty(nome);
+            this.nome = nome.trim();
+        }
+
+        public int id(){
+            return this.id;
+        }
+
+        public void setId(int id){
+            valida.validacaoId(id);
+            this.id = id;
+        }
 }
